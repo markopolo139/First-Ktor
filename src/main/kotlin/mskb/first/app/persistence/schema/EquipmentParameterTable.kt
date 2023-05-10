4385eabc85108e26d8ee9b999ce99a3f92945642
markopolo139 <marekseget@onet.pl>
2023-05-10 19:03:43 +0200

    feat(schema): add to parameters tables id

--- src/main/kotlin/mskb/first/app/persistence/schema/EquipmentParameterTable.kt
+++ src/main/kotlin/mskb/first/app/persistence/schema/EquipmentParameterTable.kt
@@ -1,12 +1,15 @@
 package mskb.first.app.persistence.schema
 
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
-object EquipmentParameterTable: Table("equipment_parameters") {
+object EquipmentParameterTable: IntIdTable("equipment_parameters") {
     val equipmentId = reference("equipment_id", EquipmentTable, onDelete = ReferenceOption.CASCADE, onUpdate = ReferenceOption.CASCADE)
     val key = varchar("key", 256)
     val value = varchar("value", 256)
 
-    override val primaryKey: PrimaryKey = PrimaryKey(equipmentId, key, name = "PK_EquipmentParameters")
+    init {
+        uniqueIndex(equipmentId, key)
+    }
 }
--- src/main/kotlin/mskb/first/app/persistence/schema/FireTruckParameterTable.kt
+++ src/main/kotlin/mskb/first/app/persistence/schema/FireTruckParameterTable.kt
@@ -1,12 +1,15 @@
 package mskb.first.app.persistence.schema
 
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
-object FireTruckParameterTable: Table("fire_truck_parameters") {
+object FireTruckParameterTable: IntIdTable("fire_truck_parameters") {
     val fireTruckId = reference("fire_truck_id", FireTruckTable, ReferenceOption.CASCADE, ReferenceOption.CASCADE)
     val key = varchar("key", 256)
     val value = varchar("value", 256)
 
-    override val primaryKey: PrimaryKey = PrimaryKey(fireTruckId, key, name = "PK_FireTruckParameters")
+    init {
+        uniqueIndex(fireTruckId, key)
+    }
 }
