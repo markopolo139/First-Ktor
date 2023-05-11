31067de433226f7c52c2984507ed5909fdec45e9
markopolo139 <marekseget@onet.pl>
2023-05-11 10:56:50 +0200

    fix(trainingRepo): add functions to save with memberEntity

--- src/main/kotlin/mskb/first/app/persistence/repositories/TrainingRepository.kt
+++ src/main/kotlin/mskb/first/app/persistence/repositories/TrainingRepository.kt
@@ -2,6 +2,7 @@ package mskb.first.app.persistence.repositories
 
 import mskb.first.app.entities.Training
 import mskb.first.app.persistence.DatabaseFactory.dbQuery
+import mskb.first.app.persistence.entities.MemberEntity
 import mskb.first.app.persistence.entities.TrainingEntity
 
 class TrainingRepository: CrudRepository<Training, String, TrainingEntity> {
@@ -9,16 +10,30 @@ class TrainingRepository: CrudRepository<Training, String, TrainingEntity> {
 
     override suspend fun getById(id: String): TrainingEntity? = dbQuery { TrainingEntity.findById(id) }
 
-    override suspend fun save(entity: Training): TrainingEntity? = dbQuery {
+    override suspend fun save(entity: Training): TrainingEntity = dbQuery {
         TrainingEntity.new(entity.id) {
             type = entity.type
             trainingDate = entity.trainingDate
             expirationDate = entity.expirationDate
+
         }
     }
 
     override suspend fun saveAll(entities: List<Training>): List<TrainingEntity> = dbQuery {
-        entities.mapNotNull { save(it) }
+        entities.map { save(it) }
+    }
+
+    suspend fun save(entity: Training, member: MemberEntity): TrainingEntity = dbQuery {
+        TrainingEntity.new(entity.id) {
+            type = entity.type
+            trainingDate = entity.trainingDate
+            expirationDate = entity.expirationDate
+            this.member = member
+        }
+    }
+
+    suspend fun saveAll(entities: List<Training>, member: MemberEntity): List<TrainingEntity> = dbQuery {
+        entities.map { save(it, member) }
     }
 
     override suspend fun update(entity: Training): Boolean = dbQuery {
