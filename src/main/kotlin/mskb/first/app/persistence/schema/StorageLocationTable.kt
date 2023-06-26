2e6b0666106dd91fcf227e98f32803a9258b9951
markopolo139 <marekseget@onet.pl>
2023-06-26 20:10:00 +0200

    feat(storageLocation): make name unique

--- src/main/kotlin/mskb/first/app/persistence/schema/StorageLocationTable.kt
+++ src/main/kotlin/mskb/first/app/persistence/schema/StorageLocationTable.kt
@@ -3,6 +3,6 @@ package mskb.first.app.persistence.schema
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object StorageLocationTable: IntIdTable("storage_location", "storage_location_id") {
-    val name = varchar("name", 256)
+    val name = varchar("name", 256).uniqueIndex()
     val default = bool("default")
 }
